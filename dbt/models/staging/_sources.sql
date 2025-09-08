8647c8390ac5968da202621c3792e67face4fd57
Jackie Directo <jacqueline.directo@insight.com>
2025-09-08 14:31:46 +0800

    Exercise 4: Implemented dbt Silver layer transformations
    - Created staging models for all data sources
    - Built comprehensive data quality tests (unique, not_null, relationships)
    - Implemented custom tests for business rules
    - Added incremental fact models with merge strategy
    - Created products snapshot for SCD2 tracking

--- dbt/models/staging/_sources.sql
+++ dbt/models/staging/_sources.sql
@@ -28,3 +28,45 @@ select * from read_parquet('{{ lake_root }}/parquet/suppliers/*.parquet');
 
 create or replace view bronze_suppliers_delta as
 select * from delta_scan('{{ lake_root }}/delta/suppliers');
+
+-- For exchange_rates
+create or replace view bronze_exchange_rates_parquet as
+select * from read_parquet('{{ lake_root }}/parquet/exchange_rates/*.parquet');
+
+create or replace view bronze_exchange_rates_delta as
+select * from delta_scan('{{ lake_root }}/delta/exchange_rates');
+
+-- For orders
+create or replace view bronze_orders_parquet as
+select * from read_parquet('{{ lake_root }}/parquet/orders/order_dt=*/*.parquet');
+
+create or replace view bronze_orders_delta as
+select * from delta_scan('{{ lake_root }}/delta/orders/order_dt=*');
+
+-- For orders_lines
+create or replace view bronze_orders_lines_parquet as
+select * from read_parquet('{{ lake_root }}/parquet/orders_lines/order_dt=*/*.parquet');
+
+create or replace view bronze_orders_lines_delta as
+select * from delta_scan('{{ lake_root }}/delta/orders_lines/order_dt=*');
+
+-- For events
+create or replace view bronze_events_parquet as
+select * from read_parquet('{{ lake_root }}/parquet/events/event_dt=*/*.parquet');
+
+create or replace view bronze_events_delta as
+select * from delta_scan('{{ lake_root }}/delta/events/event_dt=*');
+
+-- For returns
+create or replace view bronze_returns_parquet as
+select * from read_parquet('{{ lake_root }}/parquet/returns/*.parquet');
+
+create or replace view bronze_returns_delta as
+select * from delta_scan('{{ lake_root }}/delta/returns');
+
+-- For shipments
+create or replace view bronze_shipments_parquet as
+select * from read_parquet('{{ lake_root }}/parquet/shipments/*.parquet');
+
+create or replace view bronze_shipments_delta as
+select * from delta_scan('{{ lake_root }}/delta/shipments');
